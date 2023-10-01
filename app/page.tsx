3ad3da6a0ef41ebfbf47f72af3ce3a7fc3fd9005
Jashan Mago <codinghashira@gmail.com>
2023-10-01 15:26:27 +0530

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Box, Heading } from "@chakra-ui/react";
 export default function Home() {
   return (
     <Box>
-      <Heading size="4xl">Jai Shree Ram!</Heading>
+      <Heading size="4xl">Hello World!</Heading>
     </Box>
   );
 }
