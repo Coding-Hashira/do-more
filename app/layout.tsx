b4c02eaf4efaed9360ef8fe142c29b7220e311a0
Jashan Mago <codinghashira@gmail.com>
2023-10-01 15:27:11 +0530

    Styling fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "./utils/providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
