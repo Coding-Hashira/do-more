391c8a61097f1cb64804b26a605bcd092dd2f737
Jashan Mago <codinghashira@gmail.com>
2023-10-05 15:26:14 +0530

    Navbar setup

--- components/Global/Navbar.tsx
+++ components/Global/Navbar.tsx
@@ -1,12 +1,48 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
-import React from "react";
+"use client";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { MdDashboard, MdTimer } from "react-icons/md";
+import { usePathname, useRouter } from "next/navigation";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
-    <Flex px="2.5rem" background="secondaryDarkBg" shadow="nav" py="0.75rem">
-      <Box flexGrow="1">{/* Nav Buttons */}</Box>
+    <Flex
+      px="2.5rem"
+      alignItems="center"
+      background="secondaryDarkBg"
+      shadow="nav"
+      py="0.75rem"
+    >
+      <Box flexGrow="1" gap="1.5rem" display="flex" alignItems="center">
+        <Flex
+          gap="0.5rem"
+          cursor="pointer"
+          onClick={() => router.push("/")}
+          textColor={pathname === "/" ? "white" : "#A7A7A7"}
+          alignItems="center"
+        >
+          <MdDashboard className="text-lg" />{" "}
+          <Text fontWeight="600" fontSize="sm" textAlign="center">
+            Dashboard
+          </Text>
+        </Flex>
+        <Flex
+          textColor={pathname === "/pomodoro" ? "white" : "#A7A7A7"}
+          gap="0.5rem"
+          cursor="pointer"
+          onClick={() => router.push("/pomodoro")}
+          alignItems="center"
+        >
+          <MdTimer className="text-lg" />{" "}
+          <Text fontWeight="600" fontSize="sm" textAlign="center">
+            Focus Timer
+          </Text>
+        </Flex>
+      </Box>
 
       <Box>
         <Image
